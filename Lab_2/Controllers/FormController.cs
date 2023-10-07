6139af913c06ef1c063a00904eb160d828b156fa
Igor <vorobiov.777@yandex.ru>
2023-10-07 18:26:43 +0300

    Added controller for Form

---
+++ Lab_2/Controllers/FormController.cs
@@ -0,0 +1,16 @@
+﻿using Lab_2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_2.Controllers
+{
+	public class FormController : ControllerBase
+	{
+		[HttpPost]
+		[Route("/SendRequest")]
+		public IActionResult AcceptForm([FromForm] Form form)
+		{
+			return Ok(form);
+		}
+	}
+}
---
+++ Lab_2/Models/Form.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab_2.Models
+{
+	public class Form
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int? Age { get; set; }
+		public Sex Sex { get; set; }
+
+	}
+
+	public enum Sex
+	{
+		Male,
+		Female,
+		NotSelected
+	}
+}
