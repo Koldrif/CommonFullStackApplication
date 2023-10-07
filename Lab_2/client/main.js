c4749b7db753c514b77057b090ed740adadfcce7
Igor <vorobiov.777@yandex.ru>
2023-10-07 18:26:09 +0300

    Added send method

--- Lab_2/client/main.js
+++ Lab_2/client/main.js
@@ -1,4 +1,25 @@
-function SendForm()
+const sendFormAddress = "http://localhost:5176/SendRequest"
+
+async function SendForm()
 {
-    
+    let user = {
+        "Name" : "",
+        "Surname" : "",
+        "Age" : -1,
+        "Sex" : 2
+    }
+
+    user.Name = document.getElementById("name");
+    user.Surname = document.getElementById("second-name");
+    user.Age = document.getElementById("age");
+    user.Sex = document.getElementById("sex-select")
+
+    const response = await fetch(sendFormAddress, {
+        mode: "no-cors",
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    });
+    const message = await response.json();
+    document.getElementById("message").innerText = message.text;
 }
